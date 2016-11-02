8685a278993a3caabb7af9818aece008f4da9714
Agnieszka <39dz@gazeta.pl>
2016-11-02 22:49:38 +0100

    React state.

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Title extends React.Component {
     render() {
         return (
-            <p>Title</p>
+            <p>Title: {this.props.title}</p>
         );
     }
 }
